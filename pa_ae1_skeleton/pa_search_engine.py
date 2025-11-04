d998616cf22ab78d3352a1bb29a71559f3152c58
dmackayuofg <2652958m@student.gla.ac.uk>
2025-11-04 22:14:16 +0000

    feat(doc parsing): implement sanitize_word function to remove ascii chars

--- pa_ae1_skeleton/pa_search_engine.py
+++ pa_ae1_skeleton/pa_search_engine.py
@@ -67,7 +67,14 @@ def sanitize_word(word):
     Removes all non ascii characters from a given word
     """    
     newword = ""
-    <YOUR-CODE-HERE>
+    allowed = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+               "n","o","p","q","r","s","t","u","v","w","x","y","z",
+               "A","B","C","D","E","F","G","H","I","J","K","L","M",
+               "N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+               "0","1","2","3","4","5","6","7","8","9"]
+    for char in word:
+        if char in allowed:
+            newword += char
     return(newword)
 
 #%%----------------------------------------------------------------------------
